6e1a7c8eaff7bcd1ca1c40126d1570c7c7cfe54f
matheoledodo <matheo.gatto@gmail.com>
2025-06-30 14:50:05 +0200

    add email to register

--- Backend/backend/LearnCode/views.py
+++ Backend/backend/LearnCode/views.py
@@ -1,21 +1,23 @@
-from django.shortcuts import render
+
 
 # Create your views here.
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from django.contrib.auth import authenticate, login, logout
+from .forms import CustomUserCreationForm
+
 
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('home')  # Redirect to your home page
+            return redirect('home')
     else:
-        form = UserCreationForm()
+        form = CustomUserCreationForm()
     return render(request, 'register.html', {'form': form})
 
 def user_login(request):
