dcc61af2dc6e2072dd3d96d826d9521ceddcb4b0
Charbel El Hajj <charbel.elhajj@student.uclouvain.be>
2025-07-02 00:17:19 +0200

    Adding email and username prevention

--- Backend/backend/LearnCode/forms.py
+++ Backend/backend/LearnCode/forms.py
@@ -3,15 +3,37 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class CustomUserCreationForm(UserCreationForm):
+    first_name = forms.CharField(required=True)
+    last_name = forms.CharField(required=True)
     email = forms.EmailField(required=True)
+    phone_number = forms.CharField(required=True)
 
     class Meta:
         model = User
-        fields = ("username", "email", "password1", "password2")
+        fields = ("username","first_name", "last_name", "email", "phone_number", "password1", "password2")
 
     def save(self, commit=True):
         user = super().save(commit=False)
+        user.first_name = self.cleaned_data["first_name"]
+        user.last_name = self.cleaned_data["last_name"]
         user.email = self.cleaned_data["email"]
+        user.phone_number = self.cleaned_data["phone_number"]
         if commit:
             user.save()
         return user
+
+    def clean_email(self):
+        email = self.cleaned_data["email"]
+        if User.objects.filter(email = email).exists():
+            raise forms.ValidationError("Email already exists")
+
+        return email
+
+def authenticate_email(request=None, email=None, password=None, **credentials):
+    try:
+        user = User.objects.get(email=email)
+    except User.DoesNotExist:
+        return None
+    if user.check_password(password):
+        return user
+    return None
--- Backend/backend/LearnCode/views.py
+++ Backend/backend/LearnCode/views.py
@@ -1,13 +1,9 @@
-
-
 # Create your views here.
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from django.contrib.auth import authenticate, login, logout
-from .forms import CustomUserCreationForm
-
-
+from .forms import CustomUserCreationForm, authenticate_email
 
 def register(request):
     if request.method == 'POST':
@@ -25,6 +21,7 @@ def user_login(request):
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(request, username=username, password=password)
+        if user is None: user = authenticate_email(request, email=username, password=password)
         if user is not None:
             login(request, user)
             return redirect('home')  # Redirect to your home page
