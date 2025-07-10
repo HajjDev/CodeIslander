614ca56ab444b241344c2e00b707cd166b118a18
matheoledodo <matheo.gatto@gmail.com>
2025-07-10 21:51:16 +0200

    login with google

--- Backend/backend/backend/urls.py
+++ Backend/backend/backend/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('LearnCode/', include('LearnCode.urls')),
-    path("accounts/", include("allauth.urls")),
+    path('accounts/', include('allauth.urls')),
+
 ]
