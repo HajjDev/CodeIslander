102abb86e016495f388c2b5562aa2d56bb70fa9c
Charbel El Hajj <charbel.elhajj@student.uclouvain.be>
2025-09-28 11:50:20 +0200

    Adding Permissions, and some more attributes to Exercise, QCM and Theory

--- Backend/backend/CodeIslander/models/theory.py
+++ Backend/backend/CodeIslander/models/theory.py
@@ -1,10 +1,11 @@
 from django.db import models
+from .exercise import Exercise
 
 class Theory(models.Model):
     title = models.CharField(default = '', max_length=200)
     content = models.CharField(default= '')
-    example = models.CharField(default= '')
-
+    example = models.CharField(default= '', null=True, blank=True)
+    exercisesUnlocked = models.ManyToManyField(Exercise, blank=True)
 
     def __str__(self):
         return self.title
