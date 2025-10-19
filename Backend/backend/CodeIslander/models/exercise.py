880dabd3a176e8c3667875e8e0adff9e468982e8
matheoledodo <matheo.gatto@gmail.com>
2025-10-19 12:25:01 +0200

    models

--- Backend/backend/CodeIslander/models/exercise.py
+++ Backend/backend/CodeIslander/models/exercise.py
@@ -3,10 +3,10 @@ from django.db import models
 class Exercise(models.Model):
     title = models.CharField(default="", max_length=200)
     subject = models.CharField(default="")
-    example = models.CharField(default="")
-    hints = models.CharField(default="")
+    example = models.CharField(default="",blank=True, null=True)
+    hints = models.CharField(default="", blank=True, null=True)
     test_file = models.CharField(default="")
-    prompt = models.CharField(default="")
+    prompt = models.CharField(default="", blank=True, null=True)
     
     def __str__(self):
         return self.title
